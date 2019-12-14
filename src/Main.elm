36b1d7282c08564a6fb194c86e59b82a4beb0f84
Christopher Saunders <topher6345@gmail.com>
2019-12-14 14:00:03 -0800

    werewf

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,17 @@
 module Main exposing (main)
 
+import Array exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Encode as Encode
 
 
 type alias Model =
     { count : Int
+    , code : String
+    , memory : Array Int
 
     -- , registerA : Int
     }
@@ -15,7 +19,7 @@ type alias Model =
 
 initialModel : Model
 initialModel =
-    { count = 0 }
+    { count = 0, memory = initialize 256 (always 0), code = "; Simple example\n; Writes Hello World to the output\n\n    JMP start\nhello: DB \"Hello World!\" ; Variable\n       DB 0 ; String terminator\n\nstart:\n    MOV C, hello    ; Point to var \n    MOV D, 232  ; Point to output\n    CALL print\n        HLT             ; Stop execution\n\nprint:          ; print(C:*from, D:*to)\n    PUSH A\n    PUSH B\n    MOV B, 0\n.loop:\n    MOV A, [C]  ; Get char from var\n    MOV [D], A  ; Write to output\n    INC C\n    INC D  \n    CMP B, [C]  ; Check if end\n    JNZ .loop   ; jump if not\n\n    POP B\n    POP A\n    RET\n" }
 
 
 type Msg
@@ -40,8 +44,8 @@ view model =
         , div [] [ text <| String.fromInt model.count ]
         , button [ onClick Decrement ] [ text "-1" ]
         , h2 [] [ text "Registers/Flags" ]
-        , table [ style "border" "2px solid blue" ]
-            [ thead [ style "border" "2px solid blue" ]
+        , table []
+            [ thead []
                 [ th [] [ text "A" ]
                 , th [] [ text "B" ]
                 , th [] [ text "C" ]
@@ -52,7 +56,7 @@ view model =
                 , th [] [ text "C" ]
                 , th [] [ text "F" ]
                 ]
-            , tbody [ style "border" "2px solid blue" ]
+            , tbody []
                 [ tr
                     []
                     [ td [] [ text "A" ]
@@ -67,7 +71,31 @@ view model =
                     ]
                 ]
             ]
+        , h2 [] [ text "memory" ]
+        , table []
+            (foo
+                model
+            )
+        , h2 [] [ text "Code" ]
+        , textarea
+            [ cols 60
+            , rows 30
+            , value model.code
+            ]
+            []
+        , h2 [] [ text "Output" ]
+        , button [] [ text "Run" ]
+        , button [] [ text "Step" ]
+        , button [] [ text "Reset" ]
+        , button [] [ text "Assemble" ]
         ]
+
+
+foo model =
+    Array.toList <|
+        Array.map
+            (\i -> td [] [ text (String.fromInt i) ])
+            model.memory
 
 
 main : Program () Model Msg
