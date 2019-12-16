881552c4aae696ad656c04ffe28aa8eb70fc2008
Christopher Saunders <topher6345@gmail.com>
2019-12-16 08:16:21 -0800

    1576512981

--- src/Assembler.elm
+++ src/Assembler.elm
@@ -107,22 +107,3 @@ assemble string =
 
         Err problems ->
             ( Debug.toString problems, CPU.initalCPU.ram )
-
-
-
--- Split by newlines
--- Parse OPCODE
--- Parse OPCODE A
--- Parse OPCODE A, A
---WHERE A  can be a constant value 42, a register `A`, or a reference location `[42]`
---- Tokens
---lowercase letter
---uppercase letter
--- :
--- ;
--- input = "  MOV C, hello    ; Point to var "
---input = "start:"
---everything_after_semicolon = /;.*/
---token_splitters = /[\s,]+/
---input.lstrip.gsub(everything_after_semicolon, "").rstrip.split(token_splitters)
---
